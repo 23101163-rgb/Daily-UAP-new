6ff26d46b89ade1748835251bdb5f64cc79877e5
23101163-rgb <23101163@uap-bd.edu>
2025-10-20 17:23:54 +0600

    Added main django project

---
+++ daily_uap/daily_uap/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for daily_uap project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('daily_uap.accounts.urls')),
+    path('dashboard/', include('daily_uap.dashboard.urls')),  # ✅ added this line
+    path('', include('daily_uap.news.urls')),
+    path('search/', include('daily_uap.search.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
